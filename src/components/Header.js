a8c2a59f3773cf0ca626a8d3618271bc8e995b50
Leila <leila.boukella@gmail.com>
2022-11-24 01:43:13 -0700

    added navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,101 @@ import styled from 'styled-components'
 
 function Header() {
   return (
-    <div>Header</div>
+    <Nav>
+
+<Icon src="https://c.tenor.com/TYWdCAZu258AAAAC/guardians-of-the-galaxy-rocket-raccoon.gif"/>
+
+        <NavMenu>
+        <a>
+            {/* <img src='/images/project-icon.png' alt='projects'/> */}
+            <span>PROJECTS</span>
+        </a>
+
+        <a>
+            {/* <img src='/images/about-icon.png' alt='about'/> */}
+            <span>ABOUT</span>
+        </a>
+
+        <a>
+            {/* <img src='/images/contact-icon.png alt='contact'/> */}
+            <span>CONTACT</span>
+        </a>
+
+        <a>
+            {/* <img src='/images/resume-icon.png' alt='resume'/> */}
+            <span>RESUME</span>
+        </a>
+
+        </NavMenu>
+
+    </Nav>
   )
 }
 
 export default Header
+
+const Nav = styled.nav`
+height: 70px;
+background: #090b13;
+display: flex;
+align-items: center;
+padding: 0 36px;
+overflow-x: hidden;
+
+`
+
+const NavMenu = styled.div`
+    display: flex;
+    flex: 1;
+    justify-content: space-around;
+    margin-left: 25px;
+    align-items: center;
+
+    a {
+        display: flex;
+        align-items: center;
+        padding: 0 12px;
+        cursor: pointer;
+        color: white;
+
+        img {
+            height: 20px;
+        }
+
+        span {
+            font-size: 13px;
+            letter-spacing: 1.42px;
+            position: relative;
+            font-size: 20px;
+            font-weight: bold;
+
+            &:after {
+                content: "";
+                height: 2px;
+                background: white;
+                position: absolute;
+                left: 0;
+                right: 0;
+                bottom: -5px;
+                opacity: 0;
+                transform-origin: center;
+                transition: all 400ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+                transform: scaleX(0);
+            }
+        }
+
+        &:hover {
+            span:after {
+                transform: scaleX(1);
+                opacity: 1;
+            }
+        }
+    }
+`
+
+const Icon = styled.img`
+    width: 48px;
+    height: 48px;
+    border-radius: 50%;
+    cursor: pointer;
+`
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,9 +1,32 @@
 import React from 'react'
+import styled from 'styled-components'
 
 function Welcome() {
   return (
-    <div>Welcome</div>
+    <Container>
+    <img src="/images/planetst1.png" alt="" className="a"/>
+    <img src="/images/planetst2.png" alt="" className="b"/>
+    <img src="/images/planetst3.png" alt="" className="c"/>
+    <img src="/images/planetst4.png" alt="" className="d"/>
+    <h1>Welcome</h1>
+    </Container>
   )
 }
 
 export default Welcome
+
+const Container = styled.div`
+background-image: url("/images/spacetest.jpg");
+background-size:auto 100%;
+height: 700px;
+border:100px solid #273748;
+
+img {
+width:10%
+}
+
+.a {
+  
+}
+
+`
