7b39fb82ec3277629915fc44a36b9732dc8aa010
Leila <leila.boukella@gmail.com>
2022-12-28 15:21:09 -0700

    updating project cards

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,44 +9,43 @@ function Projects() {
     <Content>
         <Wrap>
             <img src="./images/movie.png" alt=""/>
-            <a href="https://jesus-orduno.github.io/movie-night-mania/" target="blank">
             <div>
-            <h4>
+            <a href="https://jesus-orduno.github.io/movie-night-mania/" target="blank">
+                <h4>View Project</h4>
+                </a>
+
+                <h4>View Repository</h4>
+                <h5>
                Languages: HTML / CSS / JavaScript
-               <br></br>
-               <br></br>
-               Ever had trouble deciding what to watch on movie night? 
-               Try Movie Night Mania! Simply enter in your 
-               genre and platform prefrences and let us find a movie for you!
-            </h4>
+                 </h5>
             </div>
             <h3>Movie Night Mania</h3>
-            </a>
         </Wrap>
         <Wrap>
-        <a href="https://springbot-game.herokuapp.com/" target="blank">
-            <img src="./images/springbot.png" alt=""/>
+
+            <img src="./images/shopshop.png" alt=""/>
             <div>
-            <h4>
+            <a href="https://mysterious-brushlands-70155.herokuapp.com/" target="blank">
+            <h4>View Project</h4>
+            </a>
+            <a href="https://github.com/zhaihan001/Team-Project3-Shop-Shop" target="blank">
+            <h4>View Repository</h4>
+            </a>
+            <h5>
                Languages: Handlebars / SCSS / JavaScript
-               <br></br>
-               <br></br>
-               Springbot is a challenging mini game built to improve your keyboard skills. Login to view highscores and compete against other players worldwide!
-            </h4>
+            </h5>
             </div>
-            <h3>Springbot</h3>
-        </a>
+            <h3>Shop-Shop</h3>
         </Wrap>
         <Wrap>
         <a href="https://leilabouk.github.io/M4-CodeQuiz/" target="blank">
             <img src="./images/quiz.png" alt=""/>
             <div>
-            <h4>
+            <h4>View Project</h4>
+            <h4>View Repository</h4>
+            <h5>
                Languages: HTML / CSS / JavaScript
-               <br></br>
-               <br></br>
-               Test your skills with this coding quiz! Robo buddy will cheer you on!
-            </h4>
+            </h5>
             </div>
             <h3>Quiz Game</h3>
         </a>
@@ -55,12 +54,11 @@ function Projects() {
         <a href="https://leilabouk.github.io/M3-PasswordGenerator/" target="blank">
             <img src="./images/passGen.png" alt=""/>
             <div>
-            <h4>
+            <h4>View Project</h4>
+            <h4>View Repository</h4>
+            <h5>
                Languages: HTML / CSS / JavaScript
-               <br></br>
-               <br></br>
-               Easily generate a secure password with this app
-            </h4>
+            </h5>
             </div>
             <h3>Password Generator</h3>
         </a>
@@ -69,24 +67,27 @@ function Projects() {
             <img src="./images/demo.png" alt=""/>
             <a href="https://github.com/LeilaBouk/M14-TechBlog" target="blank">
             <div>
-            <h4>
+            <h4>View Project</h4>
+            <h4>View Repository</h4>
+            <h5>
                Languages: Handlebars / SCSS / JavaScript
-               <br></br>
-               <br></br>
-               Bistro Board is a place for chefs to gather and share thoughts on the food industry.
-            </h4>
+            </h5>
             </div>
             <h3>Bistro Board</h3>
             </a>
         </Wrap>
         <Wrap>
-            <img src="./images/placeholder.jpg" alt=""/>
+        <a href="https://springbot-game.herokuapp.com/" target="blank">
+            <img src="./images/springbot.png" alt=""/>
             <div>
-            <h4>
-               Page unavailable
-            </h4>
+            <h4>View Project</h4>
+            <h4>View Repository</h4>
+            <h5>
+               Languages: Handlebars / SCSS / JavaScript
+            </h5>
             </div>
-            <h3>Coming Soon</h3>
+            <h3>Springbot</h3>
+        </a>
         </Wrap>
     </Content>
 </Container>
@@ -180,11 +181,29 @@ transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
         display: flex;
         justify-content: center;
         align-items: center;
+        flex-direction: column;
 
-        h4{
+        h5 {
             color: white;
-            padding: 12px;
-            font-size: 1.5vw;
+        }
+
+        h4 {
+            margin: 10px;
+            color: white;
+            padding: 8px;
+            border-radius: 20px;
+            border: 3px solid rgba(249, 249, 249, 0.1);
+            background: ${Palette.water};
+            font-size: 20px;
+            transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+      
+            &:hover {
+              background: ${Palette.red};
+            }
+          }
+
+        button{
+            color: red;
         }
        }
 
